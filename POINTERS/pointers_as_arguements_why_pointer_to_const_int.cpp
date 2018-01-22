3d9fbdad13ccf695e2022ef47f7b55d4b2605f85
Jayu8 <narrajayakrishna@outlook.com>
2018-01-22 18:55:50 -0500

    Update pointers_as_arguements_why_pointer_to_const_int.cpp

--- POINTERS/pointers_as_arguements_why_pointer_to_const_int.cpp
+++ POINTERS/pointers_as_arguements_why_pointer_to_const_int.cpp
@@ -3,29 +3,33 @@
 #include <iostream>
 using namespace std;
 
-
+// Increment value held by pointner allowed and pointer itself can also be incremented
 void increment_all (int* start, int* stop)
 {
   int * current = start;
   while (current != stop) {
-    ++(*current);  // increment value pointed
-    ++current;     // increment pointer
+    ++(*current);  
+    ++current;     
   }
 }
 
+// Increment value held by pointner NOT allowed and pointer itself can also be incremented 
 void print_all (const int* start, const int* stop)
 {
   const int * current = start;
   while (current != stop) {
     cout << *current << '\n';
-    ++current;     // increment pointer
+    ++current;     
   }
 }
 
+// Here both are considered pointers so that logic dont need any int i values for loop. else stop must be typecasted.
 int main ()
 {
   int numbers[] = {10,20,30};
-  increment_all (numbers,numbers+3);
+  increment_all (numbers,numbers+3); 
   print_all (numbers,numbers+3);
   return 0;
 }
+// Output:
+// 11 , 21, 31
